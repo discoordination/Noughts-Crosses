6ecad23d77cae2c22e97622197a14e78bae7bdae
Will W <willw@wills-mac.lan>
2018-07-23 12:16:33 +0100

    Initial Commit

---
+++ Noughts & Crosses/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Noughts & Crosses
+//
+//  Created by Will W on 23/07/2018.
+//  Copyright © 2018 Will W. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
